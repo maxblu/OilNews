2a1234c973f4769cf00ebb73381ddd41ca0a98ec
maxblu <danydelaosa@gmail.com>
2019-07-12 22:24:34 -0400

    script para cambiar la hora diaria listo y priera aproximacion a introducir datos en ES. Ver si seria mejor ya usar kibana directo y no usar el push script

---
+++ fix_cfg-file_time.py
@@ -0,0 +1,21 @@
+import datetime
+today    = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+
+today_str	 = ("start_date = " + str(today)       + " 00:00:00\n")
+tomorrow_str = ("end_date = "   + str(tomorrow)    + " 00:00:00\n")
+
+new_cfg_file_text = ""
+with open("config/config.cfg") as cfg_file:
+	for line in cfg_file.readlines():
+		if line.startswith("start_date"):
+			new_cfg_file_text += today_str
+		elif line.startswith("end_date"):
+			new_cfg_file_text += tomorrow_str
+		else:
+			new_cfg_file_text += line
+with open("config/config.cfg", 'w') as cfg_file:
+	cfg_file.write(new_cfg_file_text)	
+
+# start_date = '2019-07-04 00:00:00'
+# end_date = '2019-07-05 00:00:00'
